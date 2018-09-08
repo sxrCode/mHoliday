a09fa598ce9cbf8fea65a7871ce3ff7ce9875c69
sunxiran <maricle01@163.com>
2018-09-08 16:38:44 +0800

    显示双日历 添加模板框架

--- main.js
+++ main.js
@@ -1,59 +1,33 @@
 $(document).ready(function () {
-    var holidayApp = new HolidayApp();
+    console.log('calendar ready!');
 
-    $('#hol-left').bind('click', function () {
-        holidayApp.downMonth();
+    var month = 8;
+    $('.calendar-container').each(function (index, element) {
+        var cal = new CalenderCom(element, month + index);
     });
-
-    $('#hol-right').bind('click', function () {
-        holidayApp.upMonth();
-    });
-
 });
 
-var HolidayApp = function () {
+var CalenderCom = function (calEleContainer, month) {
     var _this = this;
-    var mdate = new Date();
+    var _calEle = calEleContainer; //日历容器
+    var _month = month; //当前月份
 
-    mdate.setDate(1);
+    var mdate = new Date();
+    mdate.setMonth(_month);
     var initDay = mdate.getDay(); //星期几
 
-
+    var calendarTmp = template('calendar-table', {});
+    var calendarTable = $(calendarTmp);
+    $(_calEle).append(calendarTable);
     render();
 
-    this.setDate = function (newDate) {
-        mdate = newDate;
-    }
-
-    /**
-     * 月份增加
-     */
-    this.upMonth = function () {
-        mdate.setMonth(mdate.getMonth() + 1);
-        initDay = mdate.getDay(); //星期几
-        mdate.setDate(1);
-
-        render();
-    }
-
-    /**
-     * 月份减少
-     */
-    this.downMonth = function () {
-        mdate.setMonth(mdate.getMonth() - 1);
-        initDay = mdate.getDay(); //星期几
-        mdate.setDate(1);
-
-        render();
-    }
-
     function render() {
-        var dateDatas = [];
 
+        var dates = [];
         for (var i = 0; i < initDay; i++) {
-            dateDatas.push({
+            dates.push({
                 type: 0, //0-空 1-工作日 2-假日
-                date: 0,
+                num: 0,
             });
         }
 
@@ -63,7 +37,7 @@ var HolidayApp = function () {
 
             var mDate = new Date(mdate.getFullYear(), mdate.getMonth(), i);
             var type = (mDate.getDay() == 0 || mDate.getDay() == 6) ? 2 : 1;
-            dateDatas.push({
+            dates.push({
                 type: type,
                 num: i,
                 date: mDate
@@ -71,76 +45,35 @@ var HolidayApp = function () {
         }
 
         for (var i = 0; i < (42 - monthDays - initDay); i++) {
-            dateDatas.push({
+            dates.push({
                 type: 0, //0-空 1-工作日 2-假日
                 date: 0,
             });
         }
 
-        var trs = $('#holidy-container .hol-date-tr');
-
-        /**
-         * 清空
-         */
-        $(trs).each(function (index, ele) {
-            $(ele).html('');
-        });
-
-
-        /**
-         * 渲染页面
-         */
-
-        for (var i = 0; i < dateDatas.length; i++) {
-            var cellData = dateDatas[i];
+        var trs = $('.calendar-tr', calendarTable);
+        console.log(trs.length);
+        for (var i = 0; i < dates.length; i++) {
+            var cellData = dates[i];
             var row = parseInt(i / 7);
+            var calendartr = trs[row];
+            var calendarCellTmp = template('calendar-cell', {});
+            var calendarCell = $(calendarCellTmp);
+            $(calendartr).append(calendarCell);
 
-            if (cellData.type == 2) {
-                var cell = $('<div class="hol-date-cell unselect holidy-cell">' + '<p>' + cellData.num + '</p>' + '</div>');
-                $(trs[row]).append(cell);
-                var dateCell = new DateCell(cellData.date, cellData.type, cell);
+            var cellBody = $('div.hol-date-cell-body', calendarCell)[0];
 
-            } else if (cellData.type == 1) {
-                $(trs[row]).append('<div class="hol-date-cell unselect workday-cell">' + '<p>' + cellData.num + '</p>' + '</div>');
-            } else {
-                $(trs[row]).append('<div class="hol-date-cell "></div>');
+            if (cellData.type != 0) {
+                $(cellBody).append($('<div class="tn-hol-body-num">' + cellData.num + '</div>'));
             }
 
+
         }
-
-        $('#hol-month').html(mdate.getFullYear() + '年' + (mdate.getMonth() + 1) + '月');
     }
-}
 
-function dealDateRow(initPos, dateNum) {
-    var fakePos = initPos + dateNum + 1;
-    var row = fakePos / 7;
-    return row;
-}
+};
 
 function mGetDate(year, month) {
     var d = new Date(year, month, 0);
     return d.getDate();
-}
-
-var DateCell = function (d, type, ele) {
-
-    var mdate = d;
-    var mtype = type;
-    var mele = ele;
-    var isSelect = false;
-    var _this = this;
-
-    this.onSelect = function () {
-        if (!isSelect) {
-            $(mele).addClass('hol-cell-selected');
-        } else {
-            $(mele).removeClass('hol-cell-selected');
-        }
-        isSelect = !isSelect;
-
-    };
-
-    $(mele).bind('click', _this.onSelect);
-
 }
